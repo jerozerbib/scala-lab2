64b74b692c2b6dc4aab2917be920753224e0f75c
Stephane Selim <stephan.selim@epfl.ch>
2020-03-30 17:37:18 +0200

    try commit

--- lab2/src/main/scala/Main.scala
+++ lab2/src/main/scala/Main.scala
@@ -7,6 +7,8 @@ object Main extends App {
 
   println("Bienvenue au Chill-Out !")
 
+  // Bla bla
+
   while (true) {
     StdIn.readLine.toLowerCase match {
       case "adieu" | "adieu." => println("À la revoyure !"); System.exit(0)
