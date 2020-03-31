62cd2cd333797fc502c5080224a152e68e33bf58
Stephane Selim <stephan.selim@epfl.ch>
2020-03-31 14:25:55 +0200

    Implement users info

--- lab2/src/main/scala/Data/UsersInfo.scala
+++ lab2/src/main/scala/Data/UsersInfo.scala
@@ -8,7 +8,18 @@ object UsersInfo {
   private var _activeUser: String = _
 
   // TODO: step 2 - create an attribute that will contain each user and its current balance.
-  private var accounts = ???
+  private var accounts = collection.mutable.Map[String, Double]()
+
+  /**
+    * Register a new user with a default account balance of 30 CHF.
+    * @param user the user to save
+    */
+  def registerUser(user: String): Unit = {
+    if (accounts.contains(user)) {
+      throw new Error("User " + user + " is already registered")
+    }
+    accounts(user) = 30
+  }
 
   /**
     * Update an account by decreasing its balance.
@@ -17,5 +28,9 @@ object UsersInfo {
     * @return the new balance
     */
   // TODO: step 2
-  def purchase(user: String, amount: Double): Double = ???
+  def purchase(user: String, amount: Double): Double = {
+    val newAmount = accounts(user) - amount
+    accounts(user) = newAmount
+    newAmount
+  }
 }
